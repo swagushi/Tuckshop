b31f779e6cd12a9f870b3f507b86e1312baa07a2
Aayush Bhandari <ac111710@avcol.school.nz>
2023-05-29 14:58:54 +1200

    Add project files.

---
+++ Tuckshop/Areas/Identity/Data/TuckshopUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tuckshop.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the TuckshopUser class
+public class TuckshopUser : IdentityUser
+{
+}
+
