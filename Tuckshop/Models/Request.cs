f3212c5bcfc3158349fbdd1d6f2d973ca7db5725
aayush bhandari <ac111710@avcol.school.nz>
2023-08-05 19:24:41 +1200

    Fixed up some things again

--- Tuckshop/Models/Request.cs
+++ Tuckshop/Models/Request.cs
@@ -5,15 +5,19 @@ namespace Tuckshop.Models
     public class Request
     {
         public int RequestID { get; set; }
+        [Required]
         [Display(Name = "Order Name")]
         [StringLength(50, MinimumLength = 3)]
         [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
          ErrorMessage = "Characters are not allowed.")]
         public string OrderName { get; set; }
+        [Required]
         [Display(Name = "Order Number")]
-        [Range(1, 1000, ErrorMessage = "Please Enter a value from 1 to 1000")]
+        [Range(1, 1000, ErrorMessage = "Please Enter a value between 1 and $1000")]
         public int OrderNumber { get; set; }
         [Display(Name = "Date Ordered")]
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime DateOrdered { get; set; }
         public ICollection<Student> Student { get; set; }
         public ICollection<Food> Food { get; set; }
