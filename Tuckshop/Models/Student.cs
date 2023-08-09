cda463f16fde28778f74c33754d0bbecd3edf92a
Aayush Bhandari <ac111710@avcol.school.nz>
2023-08-10 10:32:58 +1200

    regular expression validation has been added to students firstname and lastname

--- Tuckshop/Models/Student.cs
+++ Tuckshop/Models/Student.cs
@@ -8,10 +8,14 @@ namespace Tuckshop.Models
         [Required]
         [Display (Name="First Name")]
         [StringLength (50, MinimumLength =3)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and characters are not allowed.")]
         public string FirstName { get; set; }
         [Required]
         [Display(Name = "Last Name")]
         [StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and characters are not allowed.")]
         public string LastName { get; set; }
         [Display(Name = "Home Room")]
         
