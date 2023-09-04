a156d82e965b8bd8e0a61b26bc9c88e61f2dff98
swagushi <ac111710@avcol.school.nz>
2023-09-04 15:09:38 +1200

    working on it

--- Tuckshop/Models/Request.cs
+++ Tuckshop/Models/Request.cs
@@ -17,7 +17,9 @@ namespace Tuckshop.Models
         public int OrderNumber { get; set; }
         [Display(Name = "Date Ordered")]
         [Required]
-        [DataType(DataType.DateTime)]
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{04-09-2023}", ApplyFormatInEditMode = true)]
         public DateTime DateOrdered { get; set; }
         public ICollection<Student> Student { get; set; }
         public ICollection<Food> Food { get; set; }
