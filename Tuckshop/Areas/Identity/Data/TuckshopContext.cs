30a25db8471136228b406d817678e9f78dca4c99
aayush bhandari <ac111710@avcol.school.nz>
2023-09-09 22:05:35 +1200

    Seeded Data for Student

--- Tuckshop/Areas/Identity/Data/TuckshopContext.cs
+++ Tuckshop/Areas/Identity/Data/TuckshopContext.cs
@@ -14,22 +14,8 @@ public class TuckshopContext : IdentityDbContext<TuckshopUser>
     {
     }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
-    }
 
-    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<TuckshopUser>
-    {
-        public void Configure(EntityTypeBuilder<TuckshopUser> builder)
-        { 
-            builder.Property(u => u.Firstname).HasMaxLength(200);
-            builder.Property(u => u.LastName).HasMaxLength(200);
-        }
-    }
+   
     public DbSet<Tuckshop.Models.Request>? Request { get; set; }
 
     public DbSet<Tuckshop.Models.Food>? Food { get; set; }
@@ -37,4 +23,27 @@ public class TuckshopContext : IdentityDbContext<TuckshopUser>
     public DbSet<Tuckshop.Models.Student>? Student { get; set; }
 
     public DbSet<Tuckshop.Models.Payment>? Payment { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Student>().HasData(
+            new Student() { StudentID=1 , FirstName = "Josef", LastName = "Fatu", Homeroom = "1" },
+            new Student() { StudentID = 2, FirstName = "Rynal", LastName = "Chand", Homeroom = "1" },
+            new Student() { StudentID = 3, FirstName = "Sujal", LastName = "Chand", Homeroom = "1" }
+
+            );
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+    public class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<TuckshopUser>
+    {
+        public void Configure(EntityTypeBuilder<TuckshopUser> builder)
+        {
+            builder.Property(u => u.Firstname).HasMaxLength(200);
+            builder.Property(u => u.LastName).HasMaxLength(200);
+        }
+    }
 }
+
+
---
+++ Tuckshop/Migrations/20230909100420_seeding database.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tuckshop.Migrations
+{
+    public partial class seedingdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "AspNetUsers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentID", "FirstName", "FoodID", "Homeroom", "LastName", "PaymentID" },
+                values: new object[] { 1, "Josef", null, "1", "Fatu", null });
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentID", "FirstName", "FoodID", "Homeroom", "LastName", "PaymentID" },
+                values: new object[] { 2, "Rynal", null, "1", "Chand", null });
+
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "StudentID", "FirstName", "FoodID", "Homeroom", "LastName", "PaymentID" },
+                values: new object[] { 3, "Sujal", null, "1", "Chand", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "StudentID",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+        }
+    }
+}
